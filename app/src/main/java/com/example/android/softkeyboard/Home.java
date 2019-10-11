45c9e1d627e86193466bc34f9f2aa4f17a1c80c3
Ajnas <mailajnaskt@gmail.com>
2019-10-11 20:35:49 +0530

    Change homepage

--- app/src/main/java/com/example/android/softkeyboard/Home.java
+++ app/src/main/java/com/example/android/softkeyboard/Home.java
@@ -16,6 +16,7 @@
 
 package com.example.android.softkeyboard;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
@@ -24,37 +25,13 @@ import com.inputmethodcommon.InputMethodSettingsFragment;
 /**
  * Displays the IME preferences inside the input method setting.
  */
-public class ImePreferences extends PreferenceActivity {
-    @Override
-    public Intent getIntent() {
-        final Intent modIntent = new Intent(super.getIntent());
-        modIntent.putExtra(EXTRA_SHOW_FRAGMENT, Settings.class.getName());
-        modIntent.putExtra(EXTRA_NO_HEADERS, true);
-        return modIntent;
-    }
+public class Home extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.home);
         // We overwrite the title of the activity, as the default one is "Voice Search".
         setTitle(R.string.settings_name);
-    }
-
-    @Override
-    protected boolean isValidFragment(final String fragmentName) {
-        return Settings.class.getName().equals(fragmentName);
-    }
-
-    public static class Settings extends InputMethodSettingsFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setInputMethodSettingsCategoryTitle(R.string.language_selection_title);
-            setSubtypeEnablerTitle(R.string.select_language);
-
-            // Load the preferences from an XML resource
-            addPreferencesFromResource(R.xml.ime_preferences);
-        }
     }
 }

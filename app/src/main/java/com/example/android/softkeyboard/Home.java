faf03410d4d41543dcc0cd82f8cf0444fa9751e2
ashna <ashna@dunamisworld.com>
2019-10-14 16:19:45 +0530

    first commit

--- app/src/main/java/com/example/android/softkeyboard/Home.java
+++ app/src/main/java/com/example/android/softkeyboard/Home.java
@@ -17,10 +17,19 @@
 package com.example.android.softkeyboard;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.provider.Settings;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
 import com.inputmethodcommon.InputMethodSettingsFragment;
+
+import java.util.List;
 
 /**
  * Displays the IME preferences inside the input method setting.
@@ -33,5 +42,58 @@ public class Home extends Activity {
         setContentView(R.layout.home);
         // We overwrite the title of the activity, as the default one is "Voice Search".
         setTitle(R.string.settings_name);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String packageLocal = getPackageName();
+        boolean isInputDeviceEnabled = false;
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        List<InputMethodInfo> list = inputMethodManager.getEnabledInputMethodList();
+
+        // check if our keyboard is enabled as input method
+        for (InputMethodInfo inputMethod : list) {
+            String packageName = inputMethod.getPackageName();
+            if (packageName.equals(packageLocal)) {
+                Toast.makeText(getApplicationContext(),"Your Keyboard Enable",Toast.LENGTH_SHORT).show();
+                isInputDeviceEnabled = true;
+            }
+            else {
+                isInputDeviceEnabled = false;
+            }
+        }
+
+        if(!isInputDeviceEnabled){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
+            builder.setMessage("Enable Soft Keyboard?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            Intent enableIntent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+                            enableIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(enableIntent);
+
+                        }
+                    });
+            builder.create();
+            builder.show();
+
+        }
+        else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
+            builder.setMessage("Active Soft Keyboard?")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            InputMethodManager imeManager = (InputMethodManager) getApplicationContext().getSystemService(INPUT_METHOD_SERVICE);
+                            imeManager.showInputMethodPicker();
+
+                        }
+                    });
+            builder.create();
+            builder.show();
+        }
     }
 }
